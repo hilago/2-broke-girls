3f14231feadd81aa73878eb89ca69e5a9cae1b3a
hila go <hilagogogo@gmail.com>
2017-11-16 20:12:13 +0200

    the original site

---
+++ 2bg.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" type="image/x-icon" href="favicon1.ico" />
+<title>Max's Homemade Cupcakes</title>
+<link rel="stylesheet" type="text/css" href="2bgS.css">
+<script>
+
+
+
+</script>
+</head>
+<body>
+
+
+<div id = "wrapper">
+<div id = "flex-conteiner">
+
+
+<div id = "navbar-flex-conteiner" class="box box1">
+
+<ul class = "nav">
+<li><a href="#home"><span class="navTag">Home</span><img src="homeIc.png" alt="home" class="imgNav"></a></li>
+<li><a href ="#about"><span class="navTag">About us</span><img src="aboutIc.png" alt="about" class="imgNav"></a></li>
+<li><a href="#menu"><span class="navTag">Menu</span><img src="menueIc.png" alt="menu" class="imgNav"></a></li>
+<li><a href="#T-shirts"><span class="navTag">T-shirts</span><img src="tshirtIc.png" alt="Tshits" class="imgNav"></a></li>
+<li><a href="#contact"><span class="navTag">Contac us</span><img src="contactIc.png" alt="contact us" class="imgNav"></a></li>
+</ul>
+
+
+</div>
+
+<div class="box box2"><img class="header" src="2bg.jpg" alt="cupcakes"style="width: 100%; height:100%"/></div>
+
+<div class="box box3" id="home"><br>
+ <br><br><h2>Our Story
+</h2>
+
+<p>
+
+In 2011, Caroline started working in Brooklyn at the Williamsburg Diner.<br>
+ It was there that she met her future roommate,<br> business partner, and best friend; Max,<br>
+ waitress who has a passion and a knack for baking cupcakes.<br>
+ The girls hit it off, quickly becoming friends and roommates.<br>
+<br><br>
+
+</p>
+
+</div>
+<div id="about" class="box box4">
+<br>
+ <br><br>
+<h2>Max Black</h2>
+<p><img class="maxprofile" src="https://maxcupcakes.files.wordpress.com/2012/10/maxprofile2.jpg" alt="maxprofile"/>
+
+
+Max Black grew up in a single parent household in the Williamsburg neighborhood of Brooklyn, NY.<br>
+ She has been a waitress since 16 and started experimenting with baking at the early age of 13.<br>
+ <br><br><br><br>
+
+
+<br>
+ <br><br>
+<h2>Caroline Channing</h2>
+<p>
+<img class="carolineprofile" src="https://maxcupcakes.files.wordpress.com/2012/10/carolineprofile8.jpg" alt="carolineprofile">
+
+Caroline Westbox Channing was born in Manhattan, New York City.<br>
+Caroline was born to a wealthy family and attended the best private schools in the state.<br>
+ She is a world traveler and has even met the president.<br>
+<br>
+ <br><br>
+</div>
+
+<div id="menu"class="box box4">
+<br><br>
+<h2>Menu</h2>
+<dl> <img class="menu1" src="https://maxcupcakes.files.wordpress.com/2012/10/cupcakes.jpg" alt="cupcakes">
+ <dt>Blueberry Lemonzest</dt>
+   <dd>-Light and fluffy blueberry cupcake</dd>
+ <dt>Chocolate</dt>
+   <dd>-Rich chocolate taste with chocolate buttercream icing</dd>
+ <dt>Chocolate Curry</dt>
+   <dd>-Chocolate with a hint of curry</dd>
+  <dt>Chocolate Espresso</dt>
+   <dd>-Moist chocolate cupcake topped with espresso</dd>
+   <dt>Coconut Coffee</dt><br>
+   </div>
+
+
+<div id= "T-shirts" class="box box5">
+<br><br><h2>Order one of our T shirt <br>(touch/click for bigger scale pic)</h2>
+
+<div id="grid-biggerT">
+
+<div class="boxT boxT1">
+<div class="dropdown-img-1">
+<img class="dropdown-img-1" src="t-shirt1.png" alt="t-shirt" width="200" height="200">
+<div class="dropdown-content">
+<img src="t-shirt1.png" alt="t-shirt" width="260" height="260">
+</div>
+</div>
+</div>
+
+
+
+<div class="boxT boxT2">
+<div class="dropdown-img-2">
+<img class="dropdown-img-2" src="t-shirt2.png" alt="t-shirt" width="200" height="200">
+<div class="dropdown-content2">
+<img src="t-shirt2.png" alt="t-shirt" width="260px" height="260">
+</div>
+</div>
+</div>
+
+
+<div class="boxT boxT3">
+<div class="dropdown-img-3">
+<img class="dropdown-img-3" src="t-shirt3.png" alt="t-shirt" width="200" height="200">
+<div class="dropdown-content3">
+<img src="t-shirt3.png" alt="t-shirt" width="260" height="260">
+</div>
+</div>
+</div>
+
+</div>
+</div>
